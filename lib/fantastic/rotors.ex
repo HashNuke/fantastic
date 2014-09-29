3b3040d4b54dcde7b954eb0008e23ae4bd0745de
Akash Manohar J <akash@akash.im>
2014-09-29 16:24:30 +0530

    Fix errors

--- lib/fantastic/rotors.ex
+++ lib/fantastic/rotors.ex
@@ -2,46 +2,49 @@ defmodule Fantastic.Rotors do
   import Rotor.BasicRotors
   import CoffeeRotor
 
+
   def start(options) do
     options = determine_options(options)
     File.mkdir_p!(options[:output_path])
 
 
     rotor_options = %{manual: true}
-    Rotor.watch :coffeescripts, , fn(_changed_files, all_files)->
+    Rotor.watch :coffeescripts, options.javascript_paths, fn(_changed_files, all_files)->
       read_files(all_files)
       |> coffee
       |> concat
-      |> digested_output_to(options[:output_path])
+      |> digested_output_to(options[:output_path], "js")
     end, rotor_options
 
 
-    Rotor.watch :stylesheets, ["priv/assets/stylesheets/*.scss"], fn(_changed_files, all_files)->
+    Rotor.watch :stylesheets, options.stylesheet_paths, fn(_changed_files, all_files)->
       read_files(all_files)
       |> coffee
       |> concat
-      |> digested_output_to(options[:output_path])
+      |> digested_output_to(options[:output_path], "css")
     end, rotor_options
 
-    # TODO
-    # Use the following to list dirs in asset path
-    # Path.Wildcard.list_dir options[]
 
-    Rotor.watch :other_paths, ["priv/assets/stylesheets/*.scss"], fn(_changed_files, all_files)->
-      read_files(all_files)
-      |> coffee
-      |> concat
-      |> digested_output_to(asset_output_path)
-    end, rotor_options
+    #TODO should output the same file name with digest
+    # Rotor.watch :other_paths, other_asset_paths(options.base_path), fn(_changed_files, all_files)->
+    #   read_files(all_files)
+    #   |> coffee
+    #   |> concat
+    #   |> digested_output_to(options[:output_path])
+    # end, rotor_options
+  end
+
+
+  defp other_asset_paths(base_path) do
+    File.ls!(".")
+    |> Enum.filter(&File.dir?/1)
+    |> Enum.map( fn(dir)-> "#{base_path}/#{dir}/*" end )
   end
 
 
   defp determine_options(options) do
-    #TODO base path to replace priv/static/assets
-
     default_options = %{
-      javascript_paths: ["priv/assets/javascripts/*"],
-      stylesheet_paths: ["priv/assets/stylesheets/*"],
+      base_path: "priv/assets",
       use_digest: true,
       output_path: "priv/static/assets"
     }
@@ -51,13 +54,16 @@ defmodule Fantastic.Rotors do
 
 
   defp merge_options(options1, options2) do
-    #TODO
+    options = Map.merge(options1, options2)
+
+    Map.put(options, :javascript_paths, ["#{options.base_path}/javascripts"])
+    |> Map.put(:stylesheet_paths, ["#{options.base_path}/stylesheets"])
   end
 
 
-  defp digested_output_to(contents, asset_type, dir_path) do
+  defp digested_output_to(contents, output_dir, extension) do
     digest = calculate_digest(contents)
-    "#{dir_path}/app-#{digest}.js"
+    "#{output_dir}/app-#{digest}.#{extension}"
   end
 
 
